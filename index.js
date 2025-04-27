aee698f98c76b53abed498e70072417082b406d9
esther <esthernnyamburaa@gmail.com>
2025-04-27 19:37:33 +0300

    switch case

--- index.js
+++ index.js
@@ -1,75 +1,201 @@
-// function reversedstr (arr){
-//     return arr.map(arr.split("").sort().join(""))
-// }
-// reversedstr()
-// //     function reversed( str,indices){
-// //         return arr.map(str,index)
-// //         if indices.includes(index)
-// //       return  str.split("").reverse().join("")
-// //       return str}.sort(a,b => a-b)
-// //  }
+
+// using a for loop
+
+function sumNumbers(num){
+    let sum = 0;
+for (let i=0; i<num.length; i++){
+    sum += num[i];
+}
+return sum;
+}
+const numbers =[1,2,3,4,5,6,7,8,9];
+const sumAll = sumNumbers(numbers);
+console.log("The sum of the numbers is",sumAll);
+
+
+
+
+//using a while loop
+
+function sumUsingWhile(nums){
+let sum = 0;
+let i = 0;
+while (i<nums.length) {
+     sum
+      += nums[i];
+     i++;
+}
+return sum/2;
+}
+const myNums=[1,2,3,4,5,6,7,8,9];
+const total=sumUsingWhile(myNums);
+console.log("The sum using a while to loop is",total);
+
+
+
+function sortedGrades(arr){
+    return arr.sort(function(a,b){
+        return b-a
+    });
+   
+}
+let grade=[90,40,30,20,70,50,10,60,90];
+console.log(sortedGrades(grade));
+
+
+
+function votes(arr,){
+    let count =0
+    arr.forEach(item =>{
+        if(item.toLowerCase()==="yes"){
+            count++
+        }
+    });
+    return count;
+}
+let myArr=["yes","no","yes","yes","no","yes","yes"];
+let yesCount=votes(myArr)
+console.log(yesCount)
+
+
+function salary (arr){
+    let newsalary=5/100;
+    return 
     
-// //     let string =["esther","nyambura","queen","meron"]
-// //     console.log(reverse(arr(str,[1,2])))
-// function reversedstr(arr) {
-//     return arr.map(str => str.split('').join('')).sort();}
-//   function specificItem(arr, indices)
-//    { return arr.map((str, index) => {if (indices.includes(index)) {
-//         return str.split('').reverse().join('');}
-//       return str }).sort((a, b) => a-b);
-//     }
-    
-//   let strs = ["Thinkpad", "X1 Carbon", "Yoga", "IdeaPad"];
-//   console.log(specificItem(strs, [1 ,2]));
-
-// function addition(num){
-//     return 2+4
-
-// }
-// console.log(addition())
-
-// function filterEvenNumbers(numbers) { 
-
-//     return numbers.filter(num => num % 2 === 0); 
-  
-//   }
-//   let numbers=[1,2,3,4,5,6,7,8,9,12,45,89,145]
-//   console.log(filterEvenNumbers(numbers))
-
-//   function changingtitleCase(string){
-//   return  string.map(string =>string.split("").charAt(0).toUpperCase().slice(0).join(""))
-
-//   }
-//   let string="nyamburaEsther"
-//   console.log(changingtitleCase(string))
-
-//   Write a JS program that will join the following array elements into a string. myPets = ["Cow", "Bird", "Snake", "Dog"];
- myPets = ["Cow", "Bird", "Snake", "Dog"];
- myNewPet= myPets.join(",")
- mySorted=myPets.sort((a,b) =>a-b)
-console.log(`my pet is:${myNewPet}`)
-console.log(mySorted);
-
-let arr=[4,16,24,21,27]
- let newArr =arr.filter((item)=> item%4===0)
- console.log(newArr);
- 
-
-
- let course=["math","english","kiswahili","science","science","math"]
- let  duplicates = [];
- let uniqueArr=[];
-
- course.forEach((item) =>{
-    if(!uniqueArr.includes(item)){
-        uniqueArr.push(item) }
+}   
+let salary= [2000,3000,6000,4000,5000]                                                                                                                                    
 
 
 
-    else{
-        duplicates.push(item)
+// // given a number from 1 to 100, log 'fizz' if a number i divisible by 3 log buzz if a number is divisible by 5  log "fizzbuzz" if a number is divisible by3 and 5 else log the number 
+
+const fizzBuzz = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`Fizzbuzz`);
+        } else if (i % 3 === 0) {
+            console.log(`Fizz`);
+        } else if (i % 5 === 0) {
+            console.log(`Buzz`);
+        } else {
+            console.log(i);
+        }
     }
- )
+};
+fizzBuzz();
 
- console.log(uniqueArr)
- console.log(duplicates)
+
+// while
+
+const logInAttepts =(maxattempts=>{
+    let attempts=1;
+    while(attempts <=maxattempts){// increment befor if starts with index 0
+        console.log(`loginattempts ${attempts}`);
+        attempts++;
+    }
+})
+
+logInAttepts(3)
+
+
+
+//do while
+
+const logInAtteptsWithDoWhile =(maxattempts)=>{
+    let attempts=1;
+    do {
+        console.log(`login attempts with do... while ${attempts}`);
+        attempts++;
+    } while (attempts <= maxattempts);
+} 
+logInAtteptsWithDoWhile(3);
+
+//switch
+
+const sendNotifications=(notifications)=>{
+    notifications.forEach(notification => {
+     switch (notification) {
+        case "sms":
+            console.log("send sms ");
+            break;
+            case"push":
+            console.log("send push");
+            break;
+            case"email":
+            console.log("send email");
+            break;
+            default:
+            console.log("notification type not supported");
+            break;
+     }
+
+    });
+}
+
+
+const notifications = ["sms", "push", "email", "whatsapp"];
+
+sendNotifications(notifications);
+
+//Loop through an array of numbers from 1–7 and use a switch to print the corresponding day name.
+
+const numbers=(nums)=>{
+    nums.forEach((number)=>{
+        switch (number) {
+            case 1:
+                console.log(`Monday`)
+                break;
+                case 2:
+                console.log(`Tuesday`)
+                break;
+                case 3:
+                console.log(`Wednesday`)
+                break;
+                case 4:
+                console.log(`Thursday`)
+                break;
+                case 5:
+                console.log(`Friday`)
+                break;
+                case 6:
+                console.log(`Saturday`)
+                break;
+                case 7:
+                console.log(`Sunday`)
+                break;
+            default:
+                console.log(`Invalid number`);
+                break;
+        }
+    }); 
+}
+const nums=[1,2,3,4,5,6,7];
+numbers(nums);
+
+//Loop through an array of roles and use a switch to print the corresponding access level.
+
+const roles = ["admin", "editor", "viewer", "commentor", "guest"];
+const getAccess=(roles)=>{
+    roles.forEach((access) => {
+        switch (access) {
+            case "admin":
+                console.log(`Admin has full access`);
+                break;
+            case "editor":
+                console.log(`Editor can edit content`);
+                break;
+            case "viewer":
+                console.log(`Viewer can view content`);
+                break;
+            case "commentor":
+                console.log(`Commentor can comment on content`);
+                break;
+            default:
+                console.log(`Role not supported`);
+                break;
+        }
+    });
+};
+getAccess(roles);
+
+
